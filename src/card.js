4d87f1034f7211dd4eb527ded976576e4c2156fa
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-27 18:30:24 -0400

    Needed Files Added

---
+++ src/card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div>
+      <img src="" />
+      <div>
+        <h2>Daniel Ocampo</h2>
+        <p>do123456@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default card;
