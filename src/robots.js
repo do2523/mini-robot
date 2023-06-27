112adc0a9551c5665cf93d6401ca4af6d4361437
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-27 18:57:17 -0400

    Added random names,emails, and usernames for cards

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "John Smith",
+    username: "johnsmith",
+    email: "johnsmith@example.com",
+  },
+  {
+    id: 2,
+    name: "Michael Johnson",
+    username: "michaeljohnson",
+    email: "michaeljohnson@example.com",
+  },
+  {
+    id: 3,
+    name: "Daniel Williams",
+    username: "danielwilliams",
+    email: "danielwilliams@example.com",
+  },
+  {
+    id: 4,
+    name: "David Brown",
+    username: "davidbrown",
+    email: "davidbrown@example.com",
+  },
+  {
+    id: 5,
+    name: "James Davis",
+    username: "jamesdavis",
+    email: "jamesdavis@example.com",
+  },
+  {
+    id: 6,
+    name: "Christopher Jones",
+    username: "christopherjones",
+    email: "christopherjones@example.com",
+  },
+  {
+    id: 7,
+    name: "Matthew Wilson",
+    username: "matthewwilson",
+    email: "matthewwilson@example.com",
+  },
+  {
+    id: 8,
+    name: "Andrew Taylor",
+    username: "andrewtaylor",
+    email: "andrewtaylor@example.com",
+  },
+  {
+    id: 9,
+    name: "William Miller",
+    username: "williammiller",
+    email: "williammiller@example.com",
+  },
+  {
+    id: 10,
+    name: "Joseph Anderson",
+    username: "josephanderson",
+    email: "josephanderson@example.com",
+  },
+];
