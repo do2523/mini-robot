30bcdc139278474418e39ad30510cb44524acc5e
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-27 18:21:45 -0400

    testing component

---
+++ src/spacex.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./spacex.css";
+
+class spacex extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HELLO WORLD!!! THIS IS SPACEX</h1>
+        <p>TOOOO EZZZZZ</p>
+      </div>
+    );
+  }
+}
+export default spacex;
